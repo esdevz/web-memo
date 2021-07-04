64bd395d6fb8fa9b2b5088fec9ae930ca4b16f0a
Wael-Hx <wael.lhb@gmail.com>
2021-07-04 12:06:09 +0100

    fetch saved notes

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
+import Dexie from "dexie";
+import { useEffect } from "react";
 import Note from "./components/note/Note";
 
 const App = () => {
+  useEffect(() => {
+    const db = new Dexie("web-memo");
+    db.version(1).stores({
+      notes: "++id ,title,website,favicon,content,createdAt",
+    });
+    db.table("notes")
+      .toArray()
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }, []);
   return <Note />;
 };
 
