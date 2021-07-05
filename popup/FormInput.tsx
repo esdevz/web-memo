e8ed393b682801217facef3189cf3a777b0a692b
Wael-Hx <wael.lhb@gmail.com>
2021-07-05 16:50:15 +0100

    form input component

---
+++ popup/FormInput.tsx
@@ -0,0 +1,34 @@
+import {
+  FormControl,
+  Input,
+  FormControlProps,
+  FormLabel,
+  InputProps,
+  Textarea,
+  TextareaProps,
+} from "@chakra-ui/react";
+
+const FormInput = ({
+  id,
+  inputProps,
+  textAreaProps,
+  textField,
+  ...rest
+}: FormInputProps) => {
+  return (
+    <FormControl {...rest}>
+      <FormLabel fontWeight="medium" fontFamily="Montserrat">
+        {id}
+      </FormLabel>
+      {textField ? <Textarea {...textAreaProps} /> : <Input {...inputProps} />}
+    </FormControl>
+  );
+};
+
+export default FormInput;
+
+interface FormInputProps extends FormControlProps {
+  inputProps?: InputProps;
+  textField?: boolean;
+  textAreaProps?: TextareaProps;
+}
