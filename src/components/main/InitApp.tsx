0eb5123bb517b920011118d91a08d39ed4f6511f
Wael-Hx <wael.lhb@gmail.com>
2021-07-06 13:33:38 +0100

    init app

---
+++ src/components/main/InitApp.tsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import { FC } from "react";
+import { useCallback } from "react";
+import useNoteStore from "../../store/noteStore";
+
+const InitApp: FC = (props) => {
+  const [loading, setLoading] = useState(true);
+  const getNotes = useNoteStore((state) => state.getNotes);
+
+  const getNotesCallback = useCallback(
+    async () => await getNotes(),
+    [getNotes]
+  );
+
+  useEffect(() => {
+    getNotesCallback().then(() => setLoading(false));
+  });
+  if (loading) {
+    return <div> loading... </div>;
+  }
+  return <> {props.children} </>;
+};
+
+export default InitApp;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,16 @@
+import "@fontsource/raleway";
 import ReactDom from "react-dom";
 import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "./theme";
 import "./index.css";
+import InitApp from "./components/main/InitApp";
 
 ReactDom.render(
   <ChakraProvider resetCSS theme={theme}>
-    <App />
+    <InitApp>
+      <App />
+    </InitApp>
   </ChakraProvider>,
   document.getElementById("root")
 );
