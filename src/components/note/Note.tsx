7a0be5f1d274292d21cf8bbd47d4299482b16db3
Wael-Hx <wael.lhb@gmail.com>
2021-07-08 21:40:35 +0100

    edit note redesign

--- src/components/note/Note.tsx
+++ src/components/note/Note.tsx
@@ -5,20 +5,33 @@ import {
   useColorModeValue,
   useBoolean,
   useColorMode,
-  Button,
+  IconButton,
 } from "@chakra-ui/react";
 import NoteOptions from "./NoteOptions";
-
+import { IoOpen, IoArrowBack } from "react-icons/io5";
 import { INote } from "../../store/types";
-import { useState } from "react";
+
 import { useRef } from "react";
+import useNoteStore from "../../store/noteStore";
 
 const Note = ({ note }: NoteProps) => {
   const emColor = useColorModeValue("mediumblue", "lightskyblue");
   const [open, setOpen] = useBoolean();
-  const [editable, setEditable] = useState(false);
+  const [editNote] = useNoteStore((state) => [state.edit]);
+
   const contentRef = useRef<HTMLParagraphElement>(null);
+  const TitleRef = useRef<HTMLHeadingElement>(null);
   const { colorMode } = useColorMode();
+
+  const saveNote = () => {
+    let editedNote = {
+      ...note,
+      createdAt: Date.now(),
+      content: contentRef.current?.innerText || "...",
+      title: TitleRef.current?.innerText || "Empty note",
+    };
+    editNote(editedNote);
+  };
 
   if (note.createdAt === 0) {
     return null;
@@ -39,14 +52,28 @@ const Note = ({ note }: NoteProps) => {
       transition="all 0.2s ease-in-out"
       zIndex={open ? 2 : undefined}
       bgColor={colorMode === "dark" ? "gray.800" : "white"}
+      contentEditable={open}
+      spellCheck="false"
+      outline="0px solid transparent"
     >
       <GridItem
-        cursor="pointer"
+        display="flex"
+        alignItems="center"
+        justifyContent="flex-start"
         rowSpan={1}
         maxW={open ? "80%" : "315px"}
-        onClick={() => setOpen.toggle()}
       >
-        <Text as="h2" isTruncated>
+        {open && (
+          <IconButton
+            colorScheme="messenger"
+            size="sm"
+            onClick={setOpen.off}
+            aria-label="back"
+            icon={<IoArrowBack />}
+            mr="1"
+          />
+        )}
+        <Text ref={TitleRef} as="h2" isTruncated>
           {note.title}
         </Text>
       </GridItem>
@@ -57,8 +84,6 @@ const Note = ({ note }: NoteProps) => {
         noOfLines={open ? undefined : 6}
         whiteSpace="pre-wrap"
         rowSpan={open ? 8 : 2}
-        onDoubleClick={() => setEditable(true)}
-        contentEditable={editable}
       >
         <Text ref={contentRef} maxW="75ch">
           {note.content}
@@ -81,11 +106,15 @@ const Note = ({ note }: NoteProps) => {
           })}
         </Text>
         {open ? (
-          <Button onClick={() => console.log(contentRef.current?.innerText)}>
-            log
-          </Button>
+          <NoteOptions save={saveNote} />
         ) : (
-          <NoteOptions />
+          <IconButton
+            size="sm"
+            colorScheme="messenger"
+            icon={<IoOpen />}
+            aria-label="open"
+            onClick={() => setOpen.on()}
+          />
         )}
       </GridItem>
     </Grid>
