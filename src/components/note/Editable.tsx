d3893b6d37cd42a3c819da48b11af66119bb44c6
Wael-Hx <wael.lhb@gmail.com>
2021-07-14 16:00:18 +0100

    editable div

---
+++ src/components/note/Editable.tsx
@@ -0,0 +1,43 @@
+import { GridItem, GridItemProps } from "@chakra-ui/react";
+import { ForwardedRef, forwardRef } from "react";
+
+const Editable = forwardRef(
+  (
+    { html, isOpen, ...props }: EditableProps,
+    ref: ForwardedRef<HTMLDivElement>
+  ) => (
+    <GridItem
+      ref={ref}
+      overflow={isOpen ? "auto" : "hidden"}
+      sx={{ scrollbarWidth: "thin" }}
+      noOfLines={isOpen ? undefined : 6}
+      whiteSpace="pre-wrap"
+      rowSpan={isOpen ? 8 : 2}
+      cursor={isOpen ? undefined : "pointer"}
+      p="1.5"
+      _focusVisible={{
+        outline: "2px solid rgba(128, 128, 128, 0.34)",
+        borderRadius: "4px",
+      }}
+      contentEditable={isOpen}
+      spellCheck="false"
+      maxW="80ch"
+      fontFamily="Montserrat"
+      fontWeight="normal"
+      fontSize="0.8rem"
+      dangerouslySetInnerHTML={{
+        __html: html,
+      }}
+      {...props}
+    >
+      {props.children}
+    </GridItem>
+  )
+);
+
+export default Editable;
+
+interface EditableProps extends GridItemProps {
+  html: string;
+  isOpen: boolean;
+}
