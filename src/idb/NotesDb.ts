11aa66f4551d365a53a87487db2b06bcbefb5b7d
Wael-Hx <wael.lhb@gmail.com>
2021-07-16 19:41:47 +0100

    db name

--- src/idb/NotesDb.ts
+++ src/idb/NotesDb.ts
@@ -2,29 +2,29 @@ import Dexie from "dexie";
 import { INote } from "../store/types";
 
 const schema = "++id ,title ,website, fullUrl,createdAt",
-  DB_NAME = "notes";
+  TABLE_NAME = "notes";
 
 export class NotesDB extends Dexie {
   constructor() {
-    super("notes");
+    super("web-notes");
     this.version(1).stores({
       notes: schema,
     });
   }
 
   putNote(note: INote) {
-    return this.table<INote, number>(DB_NAME).put(note);
+    return this.table<INote, number>(TABLE_NAME).put(note);
   }
   getLastNote() {
-    return this.table<INote, number>(DB_NAME).toCollection().last();
+    return this.table<INote, number>(TABLE_NAME).toCollection().last();
   }
   getNotes() {
-    return this.table<INote, number>(DB_NAME).orderBy("createdAt").toArray();
+    return this.table<INote, number>(TABLE_NAME).orderBy("createdAt").toArray();
   }
   updateNote(id: number, newNote: Partial<INote>) {
-    return this.table<INote, number>(DB_NAME).update(id, newNote);
+    return this.table<INote, number>(TABLE_NAME).update(id, newNote);
   }
   deleteNote(id: number) {
-    return this.table<INote, number>(DB_NAME).delete(id);
+    return this.table<INote, number>(TABLE_NAME).delete(id);
   }
 }
