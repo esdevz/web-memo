af09ea52b05831383e75ff9499606751d0b55cdd
Wael-Hx <wael.lhb@gmail.com>
2021-10-17 15:57:57 +0100

    new types

--- src/store/types.ts
+++ src/store/types.ts
@@ -8,12 +8,32 @@ export interface INote {
   createdAt: number;
   isPinned: boolean;
 }
-
 interface NotificationMessage {
   type: "success" | "info" | "warning" | "error";
   message: string;
 }
 
+export type CustomIcon =
+  | "default"
+  | "book"
+  | "movie"
+  | "menu"
+  | "code"
+  | "todo"
+  | "note"
+  | "notebook"
+  | "clipboard";
+
+export type Layout = "default" | "minimized";
+
+export interface CollectionOptions {
+  displayName: string;
+  customIconType: CustomIcon;
+}
+export interface Configs {
+  tabLayout: Layout;
+  collections: Record<string, CollectionOptions>;
+}
 export interface NoteStore {
   notes: Record<string, INote[]>;
   activeTab: string;
@@ -24,4 +44,10 @@ export interface NoteStore {
   delete: (note: INote) => Promise<NotificationMessage>;
   //   deleteAll: (website: string) => Promise<void>;
   pin: (note: INote) => Promise<NotificationMessage>;
+}
+export interface ConfigStore extends Configs {
+  getConfigs: () => Promise<void>;
+  update: (id: number, cfg: Partial<Configs>) => Promise<void>;
+  updateCollection: (website: string, opts: CollectionOptions) => Promise<void>;
+  addNewCollection: (website: string, opts: CollectionOptions) => void;
 }
