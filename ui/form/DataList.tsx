19dd626d492065c118d474b4ad927bd94bc1c303
Wael-Hx <wael.lhb@gmail.com>
2021-10-19 17:35:54 +0100

    new datalist

---
+++ ui/form/DataList.tsx
@@ -0,0 +1,44 @@
+import React, { ChangeEvent } from "react";
+import { HStack, Input } from "@chakra-ui/react";
+import Select from "../shared/Select";
+import { IconList } from "../icons";
+import { CustomIcon } from "../../src/store/types";
+
+const DataList = (props: DataListProps) => {
+  return (
+    <HStack fontFamily="Raleway" fontSize="1em" spacing="1" w="65%">
+      <Select
+        currentIcon={props.icon}
+        favicon={props.favicon}
+        changeMenuIcon={props.handleIconChange}
+        options={IconList}
+      />
+      <Input
+        w="full"
+        value={props.defaultValue}
+        onChange={props.onChangeHandler}
+        name="website"
+        list="collections"
+        id="collection-list"
+        placeholder="collection name ..."
+      />
+
+      <datalist id="collections">
+        {props.collections.map((col) => (
+          <option key={col} value={col} />
+        ))}
+      </datalist>
+    </HStack>
+  );
+};
+
+export default DataList;
+
+interface DataListProps {
+  defaultValue: string;
+  onChangeHandler: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleIconChange: (opt: CustomIcon) => void;
+  collections: string[];
+  icon: CustomIcon;
+  favicon?: string;
+}
