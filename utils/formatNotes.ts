5ba88d82d6b9cbf5b6a8fe5a6e5cf308485bd0b4
Wael-Hx <wael.lhb@gmail.com>
2021-10-19 16:57:27 +0100

    new notes format

---
+++ utils/formatNotes.ts
@@ -0,0 +1,26 @@
+import { INote, Collection, Configs } from "../src/store/types";
+import { defaultNote } from "./defaults";
+
+export const formatNotes = (
+  noteArray: INote[],
+  cfg: Configs
+): Record<string, Collection> => {
+  return noteArray.concat(defaultNote).reduce(
+    (collections: Record<string, Collection>, currentNote) => {
+      collections[currentNote.website] = {
+        displayName:
+          cfg.collections[currentNote.website]?.displayName || currentNote.website,
+        customIconType: cfg.collections[currentNote.website]?.customIconType || "default",
+        notes: [currentNote, ...(collections[currentNote.website]?.notes || [])],
+      };
+      return collections;
+    },
+    {
+      notes: {
+        displayName: "notes",
+        customIconType: "default",
+        notes: [],
+      },
+    }
+  );
+};
