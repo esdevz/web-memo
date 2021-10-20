bcc72c4989b799c633b0490d9a3954dba772e820
Wael-Hx <wael.lhb@gmail.com>
2021-10-20 15:30:10 +0100

    Note container component

---
+++ src/components/note/NoteContainer.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { GridProps, Grid } from "@chakra-ui/react";
+
+const NoteContainer = (props: NoteContainerProps) => {
+  return (
+    <Grid
+      {...props}
+      pos={props.open ? "fixed" : "static"}
+      w={props.open ? "69%" : "250px"}
+      h={props.open ? "95%" : "265px"}
+      top={props.open ? 0 : undefined}
+      p="2.5"
+      as="article"
+      shadow="md"
+      templateRows="repeat(10,1fr)"
+      templateColumns="1fr"
+      gap={2}
+      m="1"
+      border="1px solid rgba(128, 128, 128, 0.34)"
+      transition="height 0.2s ease-in-out"
+      zIndex={props.open ? 2 : undefined}
+      bgColor={props.colorMode === "dark" ? "gray.800" : "white"}
+    >
+      {props.children}
+    </Grid>
+  );
+};
+
+export default NoteContainer;
+
+interface NoteContainerProps extends GridProps {
+  open: boolean;
+  colorMode: "light" | "dark";
+}
