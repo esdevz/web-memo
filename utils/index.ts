0540b180509b75fa5f5e269a2b5a8ecd03bf465c
esdevz <wael.lhb@gmail.com>
2021-12-04 15:56:16 +0100

    utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,33 +1,9 @@
 export * from "./defaults";
 export * from "./defaultState";
 export * from "./formatNotes";
-export * from "./sanitizeHtml";
-
-export function getHostName(url: string) {
-  try {
-    let hostName = new URL(url).hostname;
-    if (hostName === "") {
-      return "notes";
-    }
-    return hostName;
-  } catch {
-    return "notes";
-  }
-}
-
-export function dateString(date: number) {
-  return new Date(date).toLocaleString(navigator.language, {
-    year: "numeric",
-    month: "short",
-    weekday: "short",
-    hour: "numeric",
-    minute: "2-digit",
-  });
-}
-
-export function themeColorPalette(colors: string[]) {
-  return colors.reduce((palette: Record<number, string>, color, i) => {
-    palette[(i + 1) * 100] = color;
-    return palette;
-  }, {});
-}
+export { sanitizeHtml } from "./sanitizeHtml";
+export * from "./Date";
+export * from "./badgeColors";
+export * from "./getHostName";
+export * from "./theme";
+export * from "./replaceHtmlEntities";
