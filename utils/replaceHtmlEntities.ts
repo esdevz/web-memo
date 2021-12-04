270dcab348e945eb76fe65de84b88f0d3a097cd7
esdevz <wael.lhb@gmail.com>
2021-12-04 16:55:02 +0100

    remove extra new line

--- utils/replaceHtmlEntities.ts
+++ utils/replaceHtmlEntities.ts
@@ -10,11 +10,7 @@ export function replaceHtmlEntities(html?: string) {
   if (!html) {
     return "";
   }
-  return html.replace(/([&<>\"'])|([^\S\r\n]{1,}) /g, (match) => {
-    if (match.length > 1) {
-      return `<br/>${match}`;
-    }
-
+  return html.replace(/([&<>\"'])/g, (match) => {
     return htmlEntities[match];
   });
 }
