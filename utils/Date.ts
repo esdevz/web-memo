2e073a22ac4f4fb71a605f9f2151d26ece7aebea
esdevz <wael.lhb@gmail.com>
2022-01-28 15:29:18 +0100

    more relative date formatting

--- utils/Date.ts
+++ utils/Date.ts
@@ -33,16 +33,16 @@ export function rTime(current: number, time: number, toLocaleString?: boolean) {
     return `${hours} hour${hours > 1 ? "s" : ""} ago`;
   } else if (diff < week) {
     const days = Math.round(diff / day);
-    return `${days} day${days > 1 ? "s" : ""} ago`;
+    return days <= 1 ? "yesterday" : `${days} days ago`;
   } else if (diff < month) {
     const weeks = Math.round(diff / week);
-    return `${weeks} week${weeks > 1 ? "s" : ""} ago`;
+    return weeks <= 1 ? "last week" : `${weeks} weeks ago`;
   } else if (diff < year) {
     const months = Math.round(diff / month);
-    return `${months} month${months > 1 ? "s" : ""} ago`;
+    return months <= 1 ? "last month" : `${months} months ago`;
   } else if (diff > year) {
     const years = Math.round(diff / year);
-    return `${years} year${years > 1 ? "s" : ""} ago`;
+    return years <= 1 ? "last year" : `${years} years ago`;
   } else {
     return dateString(time);
   }
