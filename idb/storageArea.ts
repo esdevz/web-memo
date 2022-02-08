5232b91cc33575937b7d8faa46d2da3e36f737b9
esdevz <wael.lhb@gmail.com>
2022-02-08 17:47:00 +0100

    storage area

---
+++ idb/storageArea.ts
@@ -0,0 +1,22 @@
+import { INote } from "../src/store/types";
+import { defaultNote } from "../utils/defaults";
+
+export async function getTemporaryNote() {
+  return new Promise<INote>((resolve) => {
+    try {
+      chrome.storage.local.get("note", (result) => {
+        if (!("note" in result)) {
+          resolve(defaultNote);
+        }
+        resolve(result.note);
+      });
+    } catch (err) {
+      console.log(err);
+      resolve(defaultNote);
+    }
+  });
+}
+
+export function setTemporaryNote(value: INote) {
+  return chrome.storage.local.set({ note: value });
+}
