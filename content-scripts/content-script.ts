1a119b13ade1f57f433a415a2bbef67bc432bbcf
esdevz <wael.lhb@gmail.com>
2022-02-08 17:04:31 +0100

    content script

---
+++ content-scripts/content-script.ts
@@ -0,0 +1,23 @@
+console.log("loaded");
+
+const onMessageHandler = (
+  request: string,
+  _: any,
+  sendResponse: (response?: any) => void
+) => {
+  if (request === "GET_SELECTION") {
+    const selection = window.getSelection();
+    const range = selection?.getRangeAt(0);
+    const fragment = range?.cloneContents();
+
+    if (fragment) {
+      const node = document.createElement("div");
+      node.append(fragment);
+      sendResponse({ data: node.innerHTML });
+    }
+  }
+};
+
+chrome.runtime.onMessage.addListener(onMessageHandler);
+
+export {};
