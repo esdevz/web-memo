0778ae0ea3a422dce019f3aa5580fe03bd399942
esdevz <wael.lhb@gmail.com>
2022-02-11 17:00:05 +0100

    refactor build process / watch mode

--- configs/main.js
+++ configs/main.js
@@ -1,25 +1,51 @@
-const { copyFile } = require("fs");
+const { copyFile } = require("fs/promises");
 const path = require("path");
+const copyStaticFiles = require("./copyFiles");
+
+const isWatchMode = process.argv[2] && process.argv[2] === "-watch";
+
+const buildPaths = [
+  {
+    src: path.resolve(__dirname, "..", "main/index.html"),
+    dest: path.resolve(__dirname, "..", "build/main/index.html"),
+  },
+  {
+    src: path.resolve(__dirname, "..", "sidebar/index.html"),
+    dest: path.resolve(__dirname, "..", "build/sidebar/index.html"),
+  },
+];
 
 require("esbuild")
   .build({
-    entryPoints: [path.resolve(__dirname, "..", "src/index.tsx")],
-    assetNames: "../assets/[name]-[hash]",
-    outdir: path.resolve(__dirname, "..", "build/main"),
+    entryPoints: [
+      path.resolve(__dirname, "..", "main/index.tsx"),
+      path.resolve(__dirname, "..", "background-scripts/background.ts"),
+      path.resolve(__dirname, "..", "sidebar/index.tsx"),
+    ],
+    entryNames: "[dir]/[name]",
+    assetNames: "./assets/[name]-[hash]",
+    outdir: path.resolve(__dirname, "..", "build"),
     minify: true,
     target: "es2020",
     format: "esm",
     bundle: true,
+    watch: isWatchMode,
     loader: {
       ".woff": "file",
       ".woff2": "file",
     },
   })
-  .then(() => {
-    copyFile(
-      path.resolve(__dirname, "..", "src/index.html"),
-      path.resolve(__dirname, "..", "build/main/index.html"),
-      (err) => console.error(err)
-    );
-    console.log("main html file copied into build folder");
+  .then(async () => {
+    try {
+      for (const buildPath of buildPaths) {
+        await copyFile(buildPath.src, buildPath.dest);
+      }
+      console.log("main and sidebar html files copied into the build folder");
+      await copyStaticFiles();
+      if (isWatchMode) {
+        console.log("watching...");
+      }
+    } catch (err) {
+      console.error(err);
+    }
   });
