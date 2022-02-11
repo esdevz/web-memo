6395dc7446c4d271b4072827d2c357c0396e6089
esdevz <wael.lhb@gmail.com>
2022-02-11 16:47:27 +0100

    async read / copy

--- configs/copyFiles.js
+++ configs/copyFiles.js
@@ -1,23 +1,23 @@
-const { readdir, copyFile } = require("fs");
+const { readdir, copyFile } = require("fs/promises");
 const path = require("path");
 
-readdir(
-  path.resolve(__dirname, "..", "public"),
-  { withFileTypes: true },
-  (err, files) => {
-    if (err) {
+module.exports = async function copyStaticFiles() {
+  const files = await readdir(path.resolve(__dirname, "..", "public"), {
+    withFileTypes: true,
+  });
+
+  if (files.length === 0) {
+    return;
+  }
+  for (const file of files) {
+    try {
+      await copyFile(
+        path.resolve(__dirname, "..", "public", file.name),
+        path.resolve(__dirname, "..", "build", file.name)
+      );
+      console.log(`copy ${file.name} into build/${file.name}`);
+    } catch (err) {
       console.error(err);
     }
-    if (files.length === 0) {
-      return;
-    }
-    for (let file of files) {
-      copyFile(
-        path.resolve(__dirname, "..", "public", file.name),
-        path.resolve(__dirname, "..", "build", file.name),
-        (err) => console.error(err)
-      );
-    }
-    console.log("files copied from public folder to build");
   }
-);
+};
