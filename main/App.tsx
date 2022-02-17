e457a9edda11d450191f1e18d340b5285a51ab9d
esdevz <wael.lhb@gmail.com>
2022-02-17 19:16:00 +0100

    shallow compare fn , CollectionTabs cleanup

--- main/App.tsx
+++ main/App.tsx
@@ -1,12 +1,11 @@
-import React, { useCallback, useEffect, useMemo, useState } from "react";
+import React, { useCallback, useEffect, useMemo } from "react";
 import { Box, Grid, useDisclosure } from "@chakra-ui/react";
 import EmptyCollection from "./components/main/EmptyCollection";
-import Tab from "./components/tab/Tab";
 import Note from "./components/note/Note";
 import NotesContainer from "./components/note/NotesContainer";
 import NoteSection from "./components/note/NoteSection";
 import Separator from "./components/note/NoteSeparator";
-import TabContainer from "./components/tab/TabContainer";
+import CollectionTabs from "./components/tab/CollectionTabs";
 import useNoteStore from "./store/noteStore";
 import Settings from "./components/main/Settings";
 import Modal from "../ui/drawer/Modal";
@@ -15,8 +14,8 @@ import EditCollectionForm from "../ui/shared/EditCollectionForm";
 import { CollectionOptions } from "./store/types";
 import SearchNotes from "./components/main/SearchNotes";
 import Export from "./components/main/Export";
-import { AnimatePresence } from "framer-motion";
-import { db } from "./store/db";
+
+import shallow from "zustand/shallow";
 
 const App = () => {
   const [collections, activeTab, addNewNote, layout, updateCollection] = useNoteStore(
@@ -29,27 +28,15 @@ const App = () => {
         state.updateCollection,
       ],
       []
-    )
+    ),
+    shallow
   );
-
-  const [tabs, setTabs] = useState(Object.keys(collections));
 
   useEffect(() => {
     browser.runtime.onMessage.addListener(
       (request: { msg: string; collectionProps: CollectionOptions }) => {
         if (request.msg === "NEW_NOTE") {
-          addNewNote(request.collectionProps).then(() => {
-            const collectionProps = request.collectionProps;
-            setTabs((currentTabs) => {
-              if (Object.keys(collectionProps).length > 0) {
-                if (!currentTabs.includes(collectionProps.displayName)) {
-                  return currentTabs.concat(collectionProps.displayName);
-                }
-                return currentTabs;
-              }
-              return currentTabs;
-            });
-          });
+          addNewNote(request.collectionProps);
         }
       }
     );
@@ -70,10 +57,6 @@ const App = () => {
     return collections[activeTab].notes.filter((note) => !note.isPinned);
   }, [activeTab, collections]);
 
-  const updateCollectionOrder = () => {
-    db.updateCollections(tabs);
-  };
-
   return (
     <Box as="main" w="100vw" h="100vh">
       <Grid
@@ -83,25 +66,7 @@ const App = () => {
         gap={3}
         overflow="auto"
       >
-        <TabContainer
-          onReorder={setTabs}
-          values={tabs}
-          colSpan={layout === "default" ? 3 : 1}
-        >
-          <AnimatePresence>
-            {tabs.map((url) => (
-              <Tab
-                updateOrder={updateCollectionOrder}
-                value={url}
-                key={url}
-                displayName={collections[url].displayName}
-                customIconType={collections[url].customIconType}
-                website={url}
-                favicon={collections[url].favicon}
-              />
-            ))}
-          </AnimatePresence>
-        </TabContainer>
+        <CollectionTabs />
         <NotesContainer colSpan={layout === "default" ? 8 : 13}>
           <EmptyCollection collections={collections} activeTab={activeTab} />
           {pinnedNote.length > 0 && (
@@ -111,7 +76,7 @@ const App = () => {
               </Separator>
               <NoteSection>
                 {pinnedNote.map((note) => (
-                  <Note setState={setTabs} key={note.id} note={note} />
+                  <Note key={note.id} note={note} />
                 ))}
               </NoteSection>
               <Separator as="h3" colSpan={1}>
@@ -121,7 +86,7 @@ const App = () => {
           )}
           <NoteSection>
             {otherNotes.map((note) => (
-              <Note setState={setTabs} key={note.id} note={note} />
+              <Note key={note.id} note={note} />
             ))}
           </NoteSection>
         </NotesContainer>
