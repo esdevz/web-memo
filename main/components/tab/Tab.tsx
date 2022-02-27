4696310f3b9b9ca38096f328590cca6d612b5673
esdevz <wael.lhb@gmail.com>
2022-02-27 15:52:54 +0100

    animation props

--- main/components/tab/Tab.tsx
+++ main/components/tab/Tab.tsx
@@ -52,9 +52,6 @@ const Tab = (props: SidebarProps) => {
     initial: { opacity: 0 },
     animate: { opacity: 1 },
     exit: { opacity: 0 },
-    transition: {
-      duration: 0.2,
-    },
   };
 
   if (tabLayout === "minimized") {
