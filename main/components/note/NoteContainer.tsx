1854ccc6a2f675f05532cfdcd01b177880395154
esdevz <wael.lhb@gmail.com>
2022-03-04 16:10:01 +0100

    forward ref + focus ouline

--- main/components/note/NoteContainer.tsx
+++ main/components/note/NoteContainer.tsx
@@ -2,35 +2,41 @@ import React from "react";
 import { GridProps, Grid } from "@chakra-ui/react";
 import { clrSwitch } from "../../theme";
 
-const NoteContainer = (props: NoteContainerProps) => {
-  return (
-    <Grid
-      {...props}
-      tabIndex={0}
-      pos={props.open ? "fixed" : "static"}
-      w={props.open ? "75%" : "20rem"}
-      h={props.open ? "95%" : "16rem"}
-      top={props.open ? 0 : undefined}
-      p="2.5"
-      as="article"
-      shadow="md"
-      templateRows="repeat(10,1fr)"
-      templateColumns="1fr"
-      gap={2}
-      borderRadius={7}
-      m="1"
-      border="1px solid rgba(128, 128, 128, 0.34)"
-      transition="height 0.2s ease-in-out"
-      zIndex={props.open ? 2 : undefined}
-      bgColor={clrSwitch[props.colorMode][props.noteColor || "default"]}
-      _focusVisible={{
-        boxShadow: "0 0 0 3px rgba(66, 153, 225, 0.6)",
-      }}
-    >
-      {props.children}
-    </Grid>
-  );
-};
+const NoteContainer = React.forwardRef<HTMLDivElement, NoteContainerProps>(
+  (props, ref) => {
+    return (
+      <Grid
+        ref={ref}
+        {...props}
+        tabIndex={0}
+        pos={props.open ? "fixed" : "static"}
+        w={props.open ? "75%" : "20rem"}
+        h={props.open ? "95%" : "16rem"}
+        top={props.open ? 0 : undefined}
+        p="2.5"
+        as="article"
+        shadow="md"
+        templateRows="repeat(10,1fr)"
+        templateColumns="1fr"
+        gap={2}
+        borderRadius={7}
+        m="1"
+        border="1px solid rgba(128, 128, 128, 0.34)"
+        transition="height 0.2s ease-in-out"
+        zIndex={props.open ? 2 : undefined}
+        bgColor={clrSwitch[props.colorMode][props.noteColor || "default"]}
+        _focusVisible={{
+          outline: "4px solid rgba(66, 153, 225, 0.6)",
+        }}
+        _focus={{
+          outline: "4px solid rgba(66, 153, 225, 0.6)",
+        }}
+      >
+        {props.children}
+      </Grid>
+    );
+  }
+);
 
 export default NoteContainer;
 interface NoteContainerProps extends GridProps {
