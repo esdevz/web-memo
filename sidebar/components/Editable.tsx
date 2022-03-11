7ed1d5f647f8d3d0ea5bb40c94afd843d7757fce
esdevz <wael.lhb@gmail.com>
2022-03-11 16:14:53 +0100

    sidebar editable element styles

--- sidebar/components/Editable.tsx
+++ sidebar/components/Editable.tsx
@@ -8,20 +8,26 @@ const Editable = (props: BoxProps, ref: React.ForwardedRef<HTMLDivElement>) => {
       w="full"
       maxW="full"
       h="full"
-      border="1px solid rgba(128, 128, 128, 0.34)"
+      border="1px solid var(--border)"
       borderRadius="2"
-      p="1.5"
+      p="1"
       spellCheck="false"
       overflow="auto"
       lineHeight="1.55"
       css={`
         scrollbar-width: thin;
+        white-space: break-spaces;
         & ol,
         & ul {
           padding-inline: revert;
         }
         & * {
-          white-space: break-spaces;
+          white-space: pre-wrap;
+        }
+        & pre {
+          padding: 0.5em;
+          background-color: rgb(185, 185, 185, 0.15);
+          border-radius: 4px;
         }
         &:focus-visible {
           outline: 2px solid rgb(49, 130, 206);
