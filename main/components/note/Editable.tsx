ece0990d83180c2303df82e8c8a4013271b60972
esdevz <wael.lhb@gmail.com>
2022-03-11 16:15:14 +0100

    main editable Component refactor

--- main/components/note/Editable.tsx
+++ main/components/note/Editable.tsx
@@ -1,73 +1,85 @@
 import React, { ForwardedRef, forwardRef } from "react";
 import { GridItem, GridItemProps } from "@chakra-ui/react";
 
-const Editable = forwardRef(
-  (
-    { sanitizedHtml, isOpen, ...props }: EditableProps,
-    ref: ForwardedRef<HTMLDivElement>
-  ) => {
-    if (!isOpen) {
-      return (
-        <GridItem
-          overflow="hidden"
-          noOfLines={6}
-          whiteSpace="break-spaces"
-          rowSpan={7}
-          cursor={"pointer"}
-          p="1"
-          maxW="95%"
-          fontWeight="normal"
-          fontSize="0.9rem"
-          lineHeight="1.55"
-          css={`
-            & ol,
-            & ul {
-              padding-inline: revert;
-            }
-
-            &:focus-visible {
-              outline: 2px solid rgba(128, 128, 128, 0.34);
-              border-radius: 4px;
-            }
-          `}
-          dangerouslySetInnerHTML={{
-            __html: sanitizedHtml,
-          }}
-          {...props}
-        />
-      );
-    }
+const Editable = (
+  { sanitizedHtml, isOpen, ...props }: EditableProps,
+  ref: ForwardedRef<HTMLDivElement>
+) => {
+  if (!isOpen) {
     return (
       <GridItem
-        ref={ref}
-        overflow="auto"
+        overflow="hidden"
+        noOfLines={6}
         whiteSpace="break-spaces"
-        rowSpan={8}
+        rowSpan={7}
+        cursor={"pointer"}
         p="1"
-        spellCheck="false"
         maxW="95%"
         fontWeight="normal"
         fontSize="0.9rem"
         lineHeight="1.55"
         css={`
-          scrollbar-width: thin;
           & ol,
           & ul {
             padding-inline: revert;
           }
-
+          & * {
+            white-space: pre-wrap;
+          }
+          & pre {
+            padding: 0.5em;
+            background-color: rgb(185, 185, 185, 0.15);
+            border-radius: 4px;
+          }
           &:focus-visible {
             outline: 2px solid rgba(128, 128, 128, 0.34);
             border-radius: 4px;
           }
         `}
+        dangerouslySetInnerHTML={{
+          __html: sanitizedHtml,
+        }}
         {...props}
       />
     );
   }
-);
+  return (
+    <GridItem
+      ref={ref}
+      overflow="auto"
+      rowSpan={8}
+      p="1"
+      spellCheck="false"
+      maxW="95%"
+      fontWeight="normal"
+      fontSize="0.9rem"
+      lineHeight="1.55"
+      css={`
+        scrollbar-width: thin;
+        & ol,
+        & ul {
+          padding-inline: revert;
+        }
+        & pre {
+          padding: 0.5em;
+          background-color: rgb(185, 185, 185, 0.15);
+          border-radius: 4px;
+        }
+        & * {
+          white-space: pre-wrap;
+        }
 
-export default Editable;
+        &:focus-visible {
+          outline: 2px solid var(--border);
+          border-radius: 4px;
+        }
+      `}
+      {...props}
+    />
+  );
+};
+
+export default forwardRef(Editable);
 
 interface EditableProps extends GridItemProps {
   sanitizedHtml: string;
