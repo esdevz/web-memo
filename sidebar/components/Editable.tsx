f10a529ef015b9f0a5936be6ec87b33588f619de
esdevz <wael.lhb@gmail.com>
2022-03-16 15:33:26 +0100

    popup editable div refactor

--- sidebar/components/Editable.tsx
+++ sidebar/components/Editable.tsx
@@ -1,30 +1,59 @@
 import React from "react";
-import { Box } from "@chakra-ui/layout";
-import { sanitizeHtml, setBadgeTempNote } from "../../utils";
+import { Box, BoxProps } from "@chakra-ui/react";
+import { setBadgeTempNote } from "../../utils/badgeColors";
+import { Editor } from "roosterjs-editor-core";
 
-interface Props {
-  contentRef: React.RefObject<HTMLDivElement>;
-  sanitizedHtml: string;
+interface Props extends BoxProps {
+  sanitizer: (html?: string) => string;
   port: chrome.runtime.Port;
+  editor?: Editor;
 }
 
-const Editable = (props: Props) => {
+const Editable = (
+  { editor, port, sanitizer, ...props }: Props,
+  ref: React.ForwardedRef<HTMLDivElement>
+) => {
   const onPasteHandler = (e: React.ClipboardEvent<HTMLDivElement>) => {
     const data = e.clipboardData.getData("text/html");
     if (data.length !== 0) {
-      e.preventDefault();
-      setBadgeTempNote();
-      const selection = window.getSelection();
-      if (!selection?.rangeCount) return false;
+      if (editor) {
+        e.preventDefault();
+        e.stopPropagation();
+        let content = sanitizer(data).trim();
+        let range = editor?.getSelectionRange();
+        editor?.insertContent(content, {
+          position: 5,
+          range,
+        });
 
-      selection.deleteFromDocument();
-      let node = document.createElement("div");
-      node.innerHTML = sanitizeHtml(data).trim();
-      selection.getRangeAt(0).insertNode(node);
-      props.port.postMessage({
+        port.postMessage({
+          msg: "EDITING",
+          changes: {
+            content: editor?.getContent(),
+            createdAt: Date.now(),
+          },
+        });
+      }
+    }
+  };
+
+  const onDropHandler = (e: React.DragEvent<HTMLDivElement>) => {
+    const data = e.dataTransfer.getData("text/html");
+    const selection = window.getSelection();
+    if (selection && selection.toString().length > 0) {
+      return false;
+    }
+    if (editor) {
+      e.preventDefault();
+      e.stopPropagation();
+      let content = sanitizer(data).trim();
+      editor.insertContent(content, {
+        position: 1,
+      });
+      port.postMessage({
         msg: "EDITING",
         changes: {
-          content: props.contentRef.current?.innerHTML,
+          content: editor.getContent(),
           createdAt: Date.now(),
         },
       });
@@ -32,41 +61,55 @@ const Editable = (props: Props) => {
   };
 
   const onInputChange = (e: React.FormEvent<HTMLDivElement>) => {
+    /* 
+    this is not triggering
+    could interfere with editor instance
+    content wont save when the popup is closed 
+    */
     setBadgeTempNote();
-    props.port.postMessage({
+    port.postMessage({
       msg: "EDITING",
       changes: { content: e.currentTarget.innerHTML, createdAt: Date.now() },
     });
   };
+
   return (
     <Box
-      ref={props.contentRef}
+      ref={ref}
       onInput={onInputChange}
-      onPaste={onPasteHandler}
-      contentEditable="true"
+      onPasteCapture={onPasteHandler}
+      onDrop={onDropHandler}
+      contentEditable
       w="full"
       maxW="full"
       h="full"
       border="1px solid rgba(128, 128, 128, 0.34)"
       borderRadius="2"
-      p="1.5"
+      p="1"
       spellCheck="false"
       overflow="auto"
-      whiteSpace="break-spaces"
-      __css={{
-        "& *": {
-          whiteSpace: "break-spaces",
-        },
-      }}
-      lineHeight="1.7"
-      _focusVisible={{
-        outline: "2px solid rgb(49, 130, 206)",
-      }}
-      dangerouslySetInnerHTML={{
-        __html: props.sanitizedHtml,
-      }}
+      lineHeight="1.55"
+      css={`
+        scrollbar-width: thin;
+        white-space: break-spaces;
+        & ol,
+        & ul {
+          padding-inline: 2ch;
+        }
+        & * {
+          white-space: pre-wrap;
+        }
+        & pre {
+          padding: 0.5em;
+        }
+        &:focus-visible {
+          outline: 2px solid rgb(49, 130, 206);
+          border-radius: 4px;
+        }
+      `}
+      {...props}
     ></Box>
   );
 };
 
-export default Editable;
+export default React.forwardRef(Editable);
