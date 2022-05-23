b49f99df03859f893c52ee19fdbd6af76b9d56b2
esdevz <wael.lhb@gmail.com>
2022-05-24 00:26:57 +0100

    React 18 render

--- main/index.tsx
+++ main/index.tsx
@@ -1,17 +1,20 @@
 import "@fontsource/raleway";
 import React from "react";
-import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import InitApp from "./components/main/InitApp";
 import App from "./App";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import "./index.css";
-ReactDom.render(
+
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <ChakraProvider resetCSS theme={theme}>
     <InitApp>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <App />
     </InitApp>
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
