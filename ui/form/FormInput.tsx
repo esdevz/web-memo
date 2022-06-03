4d679657b302c4736751313aa1422e07cb3e3a19
esdevz <wael.lhb@gmail.com>
2022-06-03 20:11:17 +0100

    +helperText , h3 label

--- ui/form/FormInput.tsx
+++ ui/form/FormInput.tsx
@@ -7,27 +7,33 @@ import {
   InputProps,
   Textarea,
   TextareaProps,
+  FormHelperText,
+  Text,
 } from "@chakra-ui/react";
 
 const FormInput = ({
-  id,
+  label,
   inputProps,
   textAreaProps,
   textField,
+  helperText,
   ...rest
 }: FormInputProps) => {
   return (
     <FormControl {...rest}>
-      <FormLabel>{id}</FormLabel>
+      <FormLabel>
+        <Text as="h3">{label}</Text>
+      </FormLabel>
       {textField ? <Textarea {...textAreaProps} /> : <Input {...inputProps} />}
+      <FormHelperText>{helperText} </FormHelperText>
     </FormControl>
   );
 };
 
 export default FormInput;
-
 interface FormInputProps extends FormControlProps {
   inputProps?: InputProps;
   textField?: boolean;
   textAreaProps?: TextareaProps;
+  helperText?: string;
 }
