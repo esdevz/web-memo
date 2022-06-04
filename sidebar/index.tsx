35315d09b9d131f1e1845429277649c30523efd0
esdevz <wael.lhb@gmail.com>
2022-06-05 00:24:18 +0100

    react 18 render , import fonts

--- sidebar/index.tsx
+++ sidebar/index.tsx
@@ -1,15 +1,20 @@
-import "@fontsource/raleway";
+import "@fontsource/raleway/variable.css";
+import "@fontsource/raleway/variable-italic.css";
+import "@fontsource/rubik/variable.css";
+import "@fontsource/rubik/variable-italic.css";
 import React from "react";
-import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import Sidebar from "./components/Sidebar";
 import theme from "./SidebarTheme";
 import "./index.css";
 
-ReactDom.render(
+const container = document.getElementById("root");
+const root = createRoot(container!);
+
+root.render(
   <ChakraProvider theme={theme} resetCSS>
     <ColorModeScript initialColorMode={theme.config.initialColorMode} />
     <Sidebar />
-  </ChakraProvider>,
-  document.getElementById("root")
+  </ChakraProvider>
 );
