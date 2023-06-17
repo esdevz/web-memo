abfa5a156306d2dea53d988b0875dff734649caa
esdevz <wael.lhb@gmail.com>
2023-06-17 04:20:13 +0100

    update main build script

--- configs/main.js
+++ configs/main.js
@@ -1,6 +1,7 @@
 const { copyFile } = require("fs/promises");
 const path = require("path");
 const copyStaticFiles = require("./copyFiles");
+const esbuild = require("esbuild");
 
 const isWatchMode = process.argv[2] && process.argv[2] === "-watch";
 
@@ -23,38 +24,52 @@ const buildPaths = [
   },
 ];
 
-require("esbuild")
-  .build({
-    entryPoints: [
-      path.resolve(__dirname, "..", "main/index.tsx"),
-      path.resolve(__dirname, "..", "background-scripts/background.ts"),
-      path.resolve(__dirname, "..", "sidebar/index.tsx"),
-    ],
-    entryNames: "[dir]/[name]",
-    assetNames: "./assets/[name]-[hash]",
-    outdir: path.resolve(__dirname, "..", "build/firefox"),
-    minify: true,
-    target: "es2020",
-    format: "esm",
-    bundle: true,
-    watch: isWatchMode,
-    loader: {
-      ".woff": "file",
-      ".woff2": "file",
-    },
-  })
-  .then(async () => {
-    try {
-      for (const buildPath of buildPaths) {
-        await copyFile(buildPath.src, buildPath.dest);
-        console.log(`copy ${path.basename(buildPath.src)} to ${buildPath.dest} `);
-      }
-
-      await copyStaticFiles();
-      if (isWatchMode) {
-        console.log("watching...");
-      }
-    } catch (err) {
-      console.error(err);
+async function copyBuildFiles() {
+  try {
+    for (const buildPath of buildPaths) {
+      await copyFile(buildPath.src, buildPath.dest);
+      console.log(`copy ${path.basename(buildPath.src)} to ${buildPath.dest} `);
     }
-  });
+
+    await copyStaticFiles();
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+/**
+ * @type esbuild.BuildOptions
+ */
+const buildOptions = {
+  entryPoints: [
+    path.resolve(__dirname, "..", "main/index.tsx"),
+    path.resolve(__dirname, "..", "background-scripts/background.ts"),
+    path.resolve(__dirname, "..", "sidebar/index.tsx"),
+  ],
+  entryNames: "[dir]/[name]",
+  assetNames: "./assets/[name]-[hash]",
+  outdir: path.resolve(__dirname, "..", "build/firefox"),
+  minify: true,
+  target: "es2020",
+  format: "esm",
+  bundle: true,
+  loader: {
+    ".woff": "file",
+    ".woff2": "file",
+  },
+};
+
+async function main() {
+  if (isWatchMode) {
+    const ctx = await esbuild.context(buildOptions);
+    await ctx.watch();
+    await copyBuildFiles();
+    console.log("watching...");
+  } else {
+    await esbuild.build(buildOptions);
+    await copyBuildFiles();
+    console.log("built successfully");
+  }
+}
+
+main();
