27a9f9e9ed21883070db67d0d5bba6b1485d9450
esdevz <wael.lhb@gmail.com>
2023-07-05 00:56:58 +0100

    new sidebar icons wip

--- ui/icons/index.tsx
+++ ui/icons/index.tsx
@@ -1,5 +1,12 @@
 import { CustomIcon } from "../../main/store/types";
-import { IconProps, Icon } from "@chakra-ui/react";
+import {
+  IconProps,
+  Icon,
+  Tooltip,
+  IconButton,
+  CircularProgress,
+  IconButtonProps,
+} from "@chakra-ui/react";
 import {
   BsClipboard,
   BsFileEarmarkCode,
@@ -8,11 +15,27 @@ import {
 } from "react-icons/bs";
 import { BiFoodMenu, BiScreenshot, BiShoppingBag } from "react-icons/bi";
 import { RiMovieLine, RiStickyNoteFill } from "react-icons/ri";
-import { AiOutlineQrcode } from "react-icons/ai";
+import { AiOutlineQrcode, AiOutlineSave, AiOutlineClear } from "react-icons/ai";
+import { ImNewTab } from "react-icons/im";
+import { PiTabsDuotone } from "react-icons/pi";
 
 interface CollectionIconProps extends IconProps {
   customIcon: CustomIcon;
 }
+
+interface SidebarButtonProps {
+  cb?: (arg?: any) => void | Promise<void>;
+}
+
+interface SaveButtonProps extends SidebarButtonProps {
+  loading: boolean;
+}
+
+const IconButtonProps: Omit<IconButtonProps, "aria-label"> = {
+  borderRadius: "50%",
+  w: "4em",
+  h: "4em",
+};
 
 export const IconList: CustomIcon[] = [
   "screenshot",
@@ -52,4 +75,59 @@ export const CollectionIcon = ({ customIcon, ...rest }: CollectionIconProps) => 
     default:
       return null;
   }
+};
+
+export const Save = (props: SaveButtonProps) => {
+  return (
+    <Tooltip fontFamily="Raleway Variable" fontSize="1rem" label="save note">
+      <IconButton
+        {...IconButtonProps}
+        type="submit"
+        form="note-form"
+        onClick={props.cb}
+        icon={
+          props.loading ? (
+            <CircularProgress size="1em" isIndeterminate color="gray.400" />
+          ) : (
+            <AiOutlineSave size={20} />
+          )
+        }
+        aria-label="save note"
+      />
+    </Tooltip>
+  );
+};
+export const OpenInNewTab = (props: SidebarButtonProps) => {
+  return (
+    <Tooltip fontFamily="Raleway Variable" fontSize="1rem" label="Open Notes Tab">
+      <IconButton
+        {...IconButtonProps}
+        onClick={props.cb}
+        icon={<ImNewTab size={20} />}
+        aria-label="open notes tab"
+      />
+    </Tooltip>
+  );
+};
+export const SaveTabs = () => {
+  return (
+    <Tooltip fontFamily="Raleway Variable" fontSize="1rem" label="Save current Tabs">
+      <IconButton
+        {...IconButtonProps}
+        icon={<PiTabsDuotone size={20} />}
+        aria-label="save current tabs"
+      />
+    </Tooltip>
+  );
+};
+export const Clear = () => {
+  return (
+    <Tooltip fontFamily="Raleway Variable" fontSize="1rem" label="Clear Note">
+      <IconButton
+        {...IconButtonProps}
+        icon={<AiOutlineClear size={20} />}
+        aria-label="clear note"
+      />
+    </Tooltip>
+  );
 };
